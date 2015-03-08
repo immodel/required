5c3281a31a2db4ebcc51cfbbad48caa2eb8db0b4
Andrew Shaffer <darawk@gmail.com>
2015-03-07 17:56:01 -0800

    conform to new plugin style

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-module.exports = function(model) {
-  model.required = function(isRequired) {
+module.exports = function() {
+  this.required = function(isRequired) {
     return isRequired
       ? this.validator(this._requiredValidator, 'required')
       : this.removeValidator(this._requiredValidator, 'required');
   };
   
-  model._requiredValidator = model._requiredValidator || function(value) {
+  this._requiredValidator = this._requiredValidator || function(value) {
     return !! value;
   };
   
-  model.requiredValidator = function(fn) {
-    return this.use(function(model) {
-      model._requiredValidator = fn;
+  this.requiredValidator = function(fn) {
+    return this.use(function() {
+      this._requiredValidator = fn;
     });
   };
 };
